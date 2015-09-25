3134a5e13b79075270380f30bc78d9a53531b852
Steven Alford <steven.alford@collinsonlatitude.com>
2015-09-26 01:20:33 +0800

    Add initial steering classes.

---
+++ Assets/NPCs/BaseSteering.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class BaseSteering<T>
+{
+    private readonly T npc;
+
+    public T NPC
+    {
+        get
+        {
+            return npc;
+        }
+    }
+
+    protected BaseSteering(T npc)
+    {
+        this.npc = npc;
+    }
+}
---
+++ Assets/NPCs/Drone/DroneSteering.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class DroneSteering : BaseSteering<Drone>
+{
+    private readonly Drone npc;
+
+    public Vector3 ObstaclesAvoidForce(IEnumerable<Vector3> obstacles)
+    {
+        var avoidSum = Vector3.zero;
+        foreach (var obstacle in obstacles)
+        {
+            var fromObstacle = npc.transform.position - obstacle;
+            avoidSum += fromObstacle.normalized/fromObstacle.magnitude;
+        }
+        return avoidSum;
+    }
+
+    public DroneSteering(Drone npc) : base(npc)
+    {
+        this.npc = npc;
+    }
+}
