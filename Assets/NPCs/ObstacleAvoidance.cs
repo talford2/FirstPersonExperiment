18a54284e551ce3f0571f10c8026c6842bd45b68
talford2 <talford2@hotmail.com>
2015-09-26 03:23:34 +1000

    Adding obstacle avoidance to drones

---
+++ Assets/NPCs/ObstacleAvoidance.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObstacleAvoidance : MonoBehaviour
+{
+	public float Radius = 1f;
+
+	public float Weight = 0.8f;
+
+	void Start()
+	{
+	}
+
+	void Update()
+	{
+	}
+}
