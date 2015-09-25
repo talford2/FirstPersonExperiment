ab1a3938fccc0b92ff53b83ae2087484cf4e4b13
talford2 <talford2@hotmail.com>
2015-09-26 03:09:57 +1000

    Fixing teams

--- Assets/NPCs/Drone/Drone.cs
+++ Assets/NPCs/Drone/Drone.cs
@@ -2,74 +2,82 @@
 
 public class Drone : MonoBehaviour
 {
-    public BaseState<Drone> State { get; set; }
+	public BaseState<Drone> State { get; set; }
 
-    public Transform Target;
+	private int team;
+	public int Team
+	{
+		get
+		{
+			return team;
+		}
+	}
 
-    public int Team = 2;
+	public Transform Target;
 
-    public float FindTargetRadius = 20f;
+	public float FindTargetRadius = 20f;
 
-    public float LoseTargetRadius = 30f;
+	public float LoseTargetRadius = 30f;
 
-    public float AttackRadius = 5f;
+	public float AttackRadius = 5f;
 
-    public float AttackStopRadius = 3f;
+	public float AttackStopRadius = 3f;
 
-    public float AttackCruiseSpeed = 0.5f;
+	public float AttackCruiseSpeed = 0.5f;
 
-    public float ChaseSpeed = 2f;
+	public float ChaseSpeed = 2f;
 
-    public float SqrTargetDistance
-    {
-        get
-        {
-            if (Target == null)
-            {
-                return -1;
-            }
-            return (Target.transform.position - transform.position).sqrMagnitude;
-        }
-    }
+	public float SqrTargetDistance
+	{
+		get
+		{
+			if (Target == null)
+			{
+				return -1;
+			}
+			return (Target.transform.position - transform.position).sqrMagnitude;
+		}
+	}
 
-    public float SqrFindTargetRadius
-    {
-        get { return Mathf.Pow(FindTargetRadius, 2f); }
-    }
+	public float SqrFindTargetRadius
+	{
+		get { return Mathf.Pow(FindTargetRadius, 2f); }
+	}
 
-    public float SqrLoseTargetRadius
-    {
-        get { return Mathf.Pow(LoseTargetRadius, 2f); }
-    }
+	public float SqrLoseTargetRadius
+	{
+		get { return Mathf.Pow(LoseTargetRadius, 2f); }
+	}
 
-    public float SqrAttackRadius
-    {
-        get { return Mathf.Pow(AttackRadius, 2f); }
-    }
+	public float SqrAttackRadius
+	{
+		get { return Mathf.Pow(AttackRadius, 2f); }
+	}
 
-    public float SqrAttackStopRadius
-    {
-        get { return Mathf.Pow(AttackStopRadius, 2); }
-    }
+	public float SqrAttackStopRadius
+	{
+		get { return Mathf.Pow(AttackStopRadius, 2); }
+	}
 
-    private void Awake()
-    {
-        TargetingUtility.AddTarget(Team, transform);
-    }
+	private void Awake()
+	{
+		team = GetComponent<Targetable>().Team;
+		TargetingUtility.AddTarget(team, transform);
+	}
 
-    private void Start()
-    {
-        Debug.Log("Drone Start");
-        State = new DroneIdle(this);
-    }
+	private void Start()
+	{
+		Debug.Log("Drone Start");
+		State = new DroneIdle(this);
+	}
 
-    private void Update()
-    {
-        State.Update();
-    }
+	private void Update()
+	{
+		State.Update();
+	}
 
-    private void OnDestroy()
-    {
-        TargetingUtility.RemoveTarget(Team, transform);
-    }
+	private void OnDestroy()
+	{
+		TargetingUtility.RemoveTarget(team, transform);
+	}
 }
