4dea2953ed6deab0989e63b5e61704874eb845d3
talford2 <talford2@hotmail.com>
2015-09-26 01:23:12 +1000

    Idle targeting

--- Assets/NPCs/Drone/DroneIdle.cs
+++ Assets/NPCs/Drone/DroneIdle.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class DroneIdle : BaseState<Drone>
 {
+	private bool IsSearchingForTarget;
+
 	public DroneIdle(Drone npc) : base(npc)
 	{
 		Debug.Log("Idle");
@@ -10,12 +12,23 @@ public class DroneIdle : BaseState<Drone>
 
 	public override void Update()
 	{
-		if (NPC.Target == null)
+		if (NPC.Target == null && !IsSearchingForTarget)
 		{
-		    NPC.Target = TargetingUtility.GetNearest(NPC.Team, NPC.transform.position, NPC.SqrFindTargetRadius);
-		    if (NPC.Target != null)
-		        NPC.State = new DroneChase(NPC);
+			NPC.StartCoroutine(GetNewTarget(1f));
+
+			if (NPC.Target != null)
+				NPC.State = new DroneChase(NPC);
 		}
 		base.Update();
+	}
+
+	private IEnumerator GetNewTarget(float delay)
+	{
+		Debug.Log("Starting looking for dude");
+		IsSearchingForTarget = true;
+		yield return new WaitForSeconds(delay);
+		NPC.Target = TargetingUtility.GetNearest(NPC.Team, NPC.transform.position, NPC.SqrFindTargetRadius);
+		IsSearchingForTarget = false;
+		Debug.Log(NPC.name + " => " + NPC.Target.name);
 	}
 }
